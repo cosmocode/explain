25606d829cc5aa1990a82535d8502b406265ad29
Andreas Gohr <andi@splitbrain.org>
2011-02-21 10:43:49 +0100

    some reformats and cleanup

--- syntax.php
+++ syntax.php
@@ -1,7 +1,6 @@
 <?php
 if(!defined('DOKU_INC')) define('DOKU_INC',realpath(dirname(__FILE__).'/../../').'/');
 if(!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
-require_once(DOKU_PLUGIN.'syntax.php');
 
 /**
  * Explain Terms and Definitions
@@ -25,114 +24,105 @@ require_once(DOKU_PLUGIN.'syntax.php');
  */
 class syntax_plugin_explain extends DokuWiki_Syntax_Plugin {
 
-  function getInfo() {
-    return array('author' => 'Adrian Lang',
-                 'email'  => 'lang@cosmocode.de',
-                 'name'   => 'Explain Plugin',
-                 'desc'   => 'Explain predefined terms with a tooltip and optional link (previous author Marc Wäckerlin',
-                 'date'   => '2009-10-14',
-                 'url'    => 'http://www.dokuwiki.org/plugin:explain');
-  }
-
-  function getType() {
-    return 'substition';
-  }
-
-  function getSort() {
-    return 239; // before 'acronym'
-  }
-
-  function syntax_plugin_explain() {
-    // "static" not allowed in PHP4?!?
-    //if (isset($keys[0]) return; // evaluate at most once
-    $lines = @file(DOKU_CONF.'explain.conf');
-    if ($lines === false) {
-        return;
+    function getType() {
+        return 'substition';
     }
-    foreach ($lines as $line) {
-      $i = (trim(mb_substr($line, 0, 1)) !== '');
-      $line = trim($line);
-      if (empty($line)) continue;
-      $parts = explode("\t", $line);
-      if ($i) $parts[0] = utf8_strtolower($parts[0]);
-      $this->map[$parts[0]] = array('desc'   => $parts[1],
-                                    'target' => $this->link(array_slice($parts, 2)),
-                                    'i'      => $i);
+
+    function getSort() {
+        return 239; // before 'acronym'
     }
-  }
 
-  function link($targets) {
-    foreach($targets as $target) {
-      $_ret = $this->_link($target);
-      if ($_ret !== '') {
-        break;
-      }
+    function syntax_plugin_explain() {
+        // "static" not allowed in PHP4?!?
+        //if (isset($keys[0]) return; // evaluate at most once
+        $lines = @file(DOKU_CONF.'explain.conf');
+        if ($lines === false) {
+            return;
+        }
+        foreach ($lines as $line) {
+            $i = (trim(mb_substr($line, 0, 1)) !== '');
+            $line = trim($line);
+            if (empty($line)) continue;
+            $parts = explode("\t", $line);
+            if ($i) $parts[0] = utf8_strtolower($parts[0]);
+            $this->map[$parts[0]] = array('desc'   => $parts[1],
+                    'target' => $this->link(array_slice($parts, 2)),
+                    'i'      => $i);
+        }
     }
-    return $_ret;
-  }
 
-  function _link($target) {
-    /* Match an URL. */
-    static $url = '^https?://';
-    // '^(http://)?[-_[:alnum:]]+[-_.[:alnum:]]*\.[a-z]{2}'
-    // '(/[-_./[:alnum:]&%?=#]*)?';
-    if (ereg($url, $target))
-      return $target;
-
-    /* Match an internal link. */
-    list($id, $hash) = split('#', $target, 2);
-    global $ID;
-
-    $_ret = '';
-    if($ID != $id) {
-      $_ret .= wl($id);
+    function link($targets) {
+        foreach($targets as $target) {
+            $_ret = $this->_link($target);
+            if ($_ret !== '') {
+                break;
+            }
+        }
+        return $_ret;
     }
-    if($hash != '') {
-      $_ret .= '#'.$hash;
-    }
-    return $_ret;
-  }
 
-  function connectTo($mode) {
-    if (count($this->map) === 0)
-      return;
+    function _link($target) {
+        /* Match an URL. */
+        static $url = '^https?://';
+        // '^(http://)?[-_[:alnum:]]+[-_.[:alnum:]]*\.[a-z]{2}'
+        // '(/[-_./[:alnum:]&%?=#]*)?';
+        if (ereg($url, $target))
+            return $target;
 
-    $re = '(?<=^|\W)(?i:'.
-          join('|', array_map('preg_quote_cb', array_keys($this->map))).
-          ')(?=\W|$)';
+        /* Match an internal link. */
+        list($id, $hash) = split('#', $target, 2);
+        global $ID;
 
-    $this->Lexer->addSpecialPattern($re, $mode, 'plugin_explain');
-  }
+        $_ret = '';
+        if($ID != $id) {
+            $_ret .= wl($id);
+        }
+        if($hash != '') {
+            $_ret .= '#'.$hash;
+        }
+        return $_ret;
+    }
 
-  function handle($match, $state, $pos, &$handler) {
-    /* Supply the matched text in any case. */
-    $data = array('content' => $match);
-    foreach (array_keys($this->map) as $rxmatch) {
-      if ($match === $rxmatch ||
-          ($this->map[$rxmatch]['i'] && utf8_strtolower($match) === $rxmatch)) {
-        $data += $this->map[$rxmatch];
-        /* Handle only the first occurrence. */
-        unset($this->map[$rxmatch]['desc']);
-        break;
-      }
-    }
-    return $data;
-  }
+    function connectTo($mode) {
+        if (count($this->map) === 0)
+            return;
 
-  function render($format, &$renderer, $data) {
-    if(is_null($data['desc'])) {
-      $renderer->doc .= hsc($data['content']);
-      return true;
+        $re = '(?<=^|\W)(?i:'.
+                join('|', array_map('preg_quote_cb', array_keys($this->map))).
+                ')(?=\W|$)';
+
+        $this->Lexer->addSpecialPattern($re, $mode, 'plugin_explain');
     }
-    $renderer->doc .= '<a class="explain"';
-    if(($data['target']) !== '') {
-      $renderer->doc .= ' href="' . hsc($data['target']) . '"';
+
+    function handle($match, $state, $pos, &$handler) {
+        /* Supply the matched text in any case. */
+        $data = array('content' => $match);
+        foreach (array_keys($this->map) as $rxmatch) {
+            if ($match === $rxmatch ||
+                    ($this->map[$rxmatch]['i'] && utf8_strtolower($match) === $rxmatch)) {
+                $data += $this->map[$rxmatch];
+                /* Handle only the first occurrence. */
+                unset($this->map[$rxmatch]['desc']);
+                break;
+            }
+        }
+        return $data;
     }
-    $renderer->doc .= '>' . hsc($data['content']);
-    if ($data['desc'] !== '') {
-      $renderer->doc .= '<span class="tooltip">'.hsc($data['desc']).'</span>';
+
+    function render($format, &$renderer, $data) {
+        if(is_null($data['desc'])) {
+            $renderer->doc .= hsc($data['content']);
+            return true;
+        }
+        $renderer->doc .= '<a class="explain"';
+        if(($data['target']) !== '') {
+            $renderer->doc .= ' href="' . hsc($data['target']) . '"';
+        }
+        $renderer->doc .= '>' . hsc($data['content']);
+        if ($data['desc'] !== '') {
+            $renderer->doc .= '<span class="tooltip">'.hsc($data['desc']).'</span>';
+        }
+        $renderer->doc .= '</a>';
+        return true;
     }
-    $renderer->doc .= '</a>';
-    return true;
-  }
 }
