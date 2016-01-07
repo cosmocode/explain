bf7e05dfe218a343f88644f348ff2f435fff404d
Michael Große <grosse@cosmocode.de>
2016-01-07 18:53:39 +0100

    Update the script for Angua js changes

--- script.js
+++ script.js
@@ -2,21 +2,22 @@
  * Handle Explain plugin popups
  */
 function plugin_explain(obj,id,text){
+    var $obj = jQuery(obj);
     var ident = 'plg_explain__'+id;
-    var div = $(ident);
+    var div = document.getElementById(ident);
     if(!div){
         div = document.createElement('div');
         div.className = 'dokuwiki insitu-footnote JSpopup';
         div.textContent = text;
         div.innerText = text;
         div.style.position = 'absolute';
-        div.style.left = (findPosX(obj)+15)+'px';
-        div.style.top = (findPosY(obj)+15)+'px';
+        div.style.left = ($obj.offset().left + 15)+'px';
+        div.style.top = ($obj.offset().top + 15)+'px';
         div.style.zIndex = 100;
         div.id = 'plg_explain__'+id;
         document.body.appendChild(div);
 
-        addEvent(obj,'mouseout',closePopups);
+        $obj.mouseout(closePopups);
     }
     div.style.display = 'block';
 }
